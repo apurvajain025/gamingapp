1f61ed5ba0d57ea837fc46fd5e3ef543cf03459f
apurvajain025 <apurvajain025@gmail.com>
2026-03-17 13:14:08 +0530

    main

--- demo.java
+++ demo.java
@@ -1,5 +1,14 @@
 public class HelloApp {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        // Check if at least one argument is provided
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            // Default message if no argument is passed
+            System.out.println("Hello, World!");
+        }
     }
 }
