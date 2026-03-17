5f4294f55e3619928a2c0744fb83752c1bc625ef
apurvajain025 <apurvajain025@gmail.com>
2026-03-17 14:09:09 +0530

    main

--- demo.java
+++ demo.java
@@ -1,9 +1,11 @@
 public class HelloApp {
     public static void main(String[] args) {
-        String name = "World";
-        if (args.length > 0) {
-            name = args[0];
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
         }
-        System.out.println("Hello, " + name + "!");
+        String result = String.join(", ", args);
+        System.out.println("Hello, " + result + "!");
     }
 }
